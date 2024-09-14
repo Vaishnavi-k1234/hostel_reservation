944b143ab55e1f73c66af878aa1e394619bacf6d
Vaishnavi-k1234 <157566376+Vaishnavi-k1234@users.noreply.github.com>
2024-09-14 18:39:09 +0530

    commit

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');//express is used to create web server
 const readline = require('readline');//readline 
 const mongoose = require('mongoose');//used to connect with MongoDB
 const app = express();
-const MongoDBURI=process.env.MONGO_URI || 'mongodb+srv://keshavapatnamvaishnavi2002:<SamsungF22User>@cluster0.8cawc.mongodb.net/hostel?retryWrites=true&w=majority&appName=Cluster0';
-//'mongodb://127.0.0.1:27017/hostel';
+const MongoDBURI=process.env.MONGO_URI ||'mongodb://127.0.0.1:27017/hostel';
 //.connect connects to the MongoDB database hostel running on the localhost.BvODxSQWP6Q3ivu3
 mongoose.connect(MongoDBURI, 
   {
